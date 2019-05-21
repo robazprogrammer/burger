178f0251020279ed1261c5ba88ac226db5263fe9
robazprogrammer <robazprogrammer@gmail.com>
2019-05-20 21:31:53 -0700

    recreate files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green Chile Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
